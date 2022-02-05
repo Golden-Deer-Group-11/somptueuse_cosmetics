60fe1e5622e9bcc90fa5d24d0723c0242fba5b60
taemagura <72679657+taemagura@users.noreply.github.com>
2022-02-05 11:12:07 +0800

    Admin - Clients: Delete

--- admin/after_login.php
+++ admin/after_login.php
@@ -1,11 +1,18 @@
 <?php include "header.php"; ?>
+<?php include "connect.php"; ?>
+
 <div class="content">
-	<br><br>
-	<?php include  "connect.php"; ?>
+	<?php
+		$a = isset($_GET['a']) ? $_GET['a']: null;
+		mysqli_query($con,"delete from checkout where id='$a'");
+
+	?>
+		<br><br>
 	<table border=1 cellpadding="6" cellspacing="8">
 		<tr>
-			 <th colspan="8">CLIENT'S ORDERS</th>
+			 <th colspan="8">PARCEL CLIENTS</th>
 		</tr>
+
 		<tr>
 			<th>Product ID</th>
 			<th>User ID</th>
@@ -16,25 +23,27 @@
 			<th>View Product</th>
 			<th>Delete</th>
 		</tr>
-		<?php
-		$s = mysqli_query($con,"select * from checkout");
-		while($r = mysqli_fetch_array($s))
-		{
+		<?php 
+			$s = mysqli_query($con,"select * from checkout");
+			while($r = mysqli_fetch_array($s))
+			{
+			?>
+				<tr align=center>
+					<td><?php echo $r['p_id']; ?></td>
+					<td><?php echo $r['u_id']; ?></td>
+					<td><?php echo $r['name']; ?></td>
+					<td><?php echo $r['mobile']; ?></td>
+					<td><?php echo $r['email']; ?></td>
+					<td><?php echo $r['location']; ?></td>
+					<td><a href="viewcart.php?pid=<?php echo $r['p_id']; ?>&uid=<?php echo $r['u_id']; ?>">View Product</a></td>
+					<td><a href="after_login.php?a=<?php echo $r['id']; ?>" class="del">Delete</a></td>
+				</tr>	
+		<?php	
+			}
 		?>
-		<tr>
-			<td><?php echo $r['p_id']; ?></td>
-			<td><?php echo $r['u_id']; ?></td>
-			<td><?php echo $r['name']; ?></td>
-			<td><?php echo $r['mobile']; ?></td>
-			<td><?php echo $r['email']; ?></td>
-			<td><?php echo $r['location']; ?></td>
-			<td><a href="viewcart.php?pid=<?php echo $r['p_id']; ?>&uid=<?php echo $r['u_id']; ?>">View Product</a></td>
-			<td><a href="">Delete</a></td>
-		<?php
-		}
-		?>
-	</table>
-	 
+
+
+	</table>	
 
 </div>
 <?php include "footer.php"; ?>
